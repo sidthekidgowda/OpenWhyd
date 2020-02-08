e7cd4cf66e07b4139532997334c874a21841a43c
Sid Gowda <sidgowda.9@gmail.com>
2020-02-08 17:01:50 -0500

    finished cache implementation to minimize number of requests

--- app/src/main/java/com/openwhyd/datasource/HotTracksDataSourceImpl.kt
+++ app/src/main/java/com/openwhyd/datasource/HotTracksDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.openwhyd.datasource
 import com.openwhyd.model.HotTrackRes
 import com.openwhyd.service.HotTrackService
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 import org.apache.commons.lang3.StringUtils
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -11,10 +12,25 @@ import javax.inject.Singleton
 class HotTracksDataSourceImpl @Inject constructor(private val hotTrackService: HotTrackService)
     : HotTracksDataSource {
 
+    private val hotTrackMapCache = mutableMapOf<String, HotTrackRes>()
+
     override fun getHotTracks(genre: String, skip: Int): Single<HotTrackRes> {
         val formattedPath =
             genre.split(StringUtils.SPACE)
                 .joinToString(separator = StringUtils.EMPTY) {it.toLowerCase()}
-        return hotTrackService.getHotTracks(formattedPath, skip)
+
+        //if key exists in cache return the value
+        //only make service call if key does not exist
+        // or load more is requested.
+
+        val shouldRerieveFromCache = hotTrackMapCache.containsKey(genre) &&
+                hotTrackMapCache[genre]?.tracks?.size != skip
+
+        if (shouldRerieveFromCache) {
+            return Single.just(hotTrackMapCache[genre]!!).subscribeOn(Schedulers.io())
+        } else {
+            return hotTrackService.getHotTracks(formattedPath, skip)
+                .doOnSuccess{ hotTrackRes: HotTrackRes? ->  hotTrackMapCache.put(genre, hotTrackRes!!)}
+        }
     }
 }
