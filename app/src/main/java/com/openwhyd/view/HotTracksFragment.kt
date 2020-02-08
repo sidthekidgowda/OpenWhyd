5498200f96df8a176c2dc438c058a7e88bf7a35d
Sid Gowda <sidgowda.9@gmail.com>
2020-02-07 21:55:28 -0500

    finished setting up fragment

--- app/src/main/java/com/openwhyd/view/HotTracksFragment.kt
+++ app/src/main/java/com/openwhyd/view/HotTracksFragment.kt
@@ -1,6 +1,25 @@
 package com.openwhyd.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.openwhyd.R
+import kotlinx.android.synthetic.main.hot_tracks_list.*
 
 class HotTracksFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.hot_tracks_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loading_spinner.visibility = View.VISIBLE
+    }
 }
