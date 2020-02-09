eef4051d0a84cde17378676490dac114cad11885
Sid Gowda <sidgowda.9@gmail.com>
2020-02-09 15:44:41 -0500

    added ViewModel test

---
+++ app/src/test/java/com/openwhyd/viewModel/HotTracksViewModelTest.kt
@@ -0,0 +1,13 @@
+package com.openwhyd.viewModel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class HotTracksViewModelTest {
+
+    @Rule
+    val testRule = InstantTaskExecutorRule()
+}
