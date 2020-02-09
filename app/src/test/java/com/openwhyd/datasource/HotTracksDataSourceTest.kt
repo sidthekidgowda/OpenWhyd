3548d906fc5c6a328c7b6838c54b26bdb6aa3ca1
Sid Gowda <sidgowda.9@gmail.com>
2020-02-09 10:43:04 -0500

    added test cases for first service call and error case

--- app/src/test/java/com/openwhyd/datasource/HotTracksDataSourceTest.kt
+++ app/src/test/java/com/openwhyd/datasource/HotTracksDataSourceTest.kt
@@ -1,10 +1,14 @@
 package com.openwhyd.datasource
 
 import com.openwhyd.service.HotTrackService
+import com.openwhyd.util.JsonUtil
+import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -27,17 +31,51 @@ class HotTracksDataSourceTest {
 
     @Test
     fun `test getHotTracks service call is made for first run`() {
-        
+        `when`(
+            hotTracksService.getHotTracks(
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyInt()
+            )
+        ).thenReturn(Single.just(JsonUtil.firstNetworkCall()))
+
+        hotTracksDataSource.getHotTracks("rock")
+            .test()
+            .assertValue(JsonUtil.firstNetworkCall())
+            .dispose()
     }
 
     @Test
-    fun `test getHotTracks uses cache for second run`() {
+    fun `test getHotTracks returns network error`() {
+        `when`(
+            hotTracksService.getHotTracks(
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyInt()
+            )
+        ).thenReturn(Single.error(Throwable("error")))
+
+        hotTracksDataSource.getHotTracks("hiphop")
+            .test()
+            .assertFailure(Throwable::class.java)
+            .dispose()
+    }
+
+    @Test
+    fun `test getHotTracks uses cache for subsequent run`() {
 
     }
 
     @Test
-    fun `cache exist`() {
+    fun `test Cache exist for different genres`() {
 
     }
 
+    @Test
+    fun `test getHotTracks when load more button is clicked makes second network call`() {
+
+    }
+
+    @Test
+    fun `test getTrackDetails returns from cache`() {
+
+    }
 }
